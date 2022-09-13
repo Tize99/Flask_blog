5a3c3ac4e6ba99119701f991fa0aa38f434a024b
user <andrei-sinyac@mail.ru>
2022-09-13 15:56:36 +0300

    db

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_UPI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
