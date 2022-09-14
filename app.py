528e8ce9398e63ae2365b7d11a108ca0ab329516
user <andrei-sinyac@mail.ru>
2022-09-14 17:45:43 +0300

    End_files

--- app.py
+++ app.py
@@ -30,18 +30,55 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/posts')
+@app.route('/posts/')
 def posts():
-    articles = Article.query.order_by(Article.date).all()
+    articles = Article.query.order_by(Article.date.desc()).all()
     return render_template("posts.html", articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def posts_detail(id):
+    article = Article.query.get(id)
+    return render_template("posts_detail.html", article=article)
+
+
+@app.route('/posts/<int:id>/delete')
+def posts_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "При удалении статьи произошла ошибка"
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "При добавлении статьи произошла ошибка"
+
+    else:
+        article = Article.query.get(id)
+        return render_template("post_update.html", article=article)
 
 
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
     if request.method == "POST":
         title = request.form['title']
-        intro = request.form['title']
-        text = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
 
         article = Article(title=title, intro=intro, text=text)
 
@@ -53,7 +90,7 @@ def create_article():
             return "При добавлении статьи произошла ошибка"
 
     else:
-        return render_template("create-article.html")
+        return render_template("create_article.html")
 
 
 if __name__ == '__main__':
